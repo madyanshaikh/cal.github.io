52975a1b0bb734eed0b71ba4b58d41f29907f500
madyanshaikh <57924238+madyanshaikh@users.noreply.github.com>
2021-03-06 03:13:17 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+  var backspace = function(){
+
+    var del= document.getElementById('display').value;
+    document.getElementById('display').value=del.substring(0,del.length-1);
+
+
+
+  }
+
